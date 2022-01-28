0328f4ba707a8013d6c6e0e43b79b8540564c6f5
Olia Dovbush <oliaivanchushun@gmail.com>
2022-01-28 20:06:45 +0200

    add seconds and minutes

--- js/main.js
+++ js/main.js
@@ -1,53 +1,52 @@
-import { millisecondsToHours, isLeapYear, getYear, getHours, isPast, parseISO } from 'date-fns';
+import {
+  millisecondsToSeconds, millisecondsToMinutes, millisecondsToHours, getHours, isPast, parseISO,
+} from 'date-fns';
 
 let countDown;
 
 const UI_ELEM = {
   CLOCK: document.querySelector('.count__clock'),
   SECONDS: document.querySelector('.count__seconds'),
+  MINUTES: document.querySelector('.count__minutes'),
   HOURS: document.querySelector('.count__hours'),
   DAYS: document.querySelector('.count__days'),
-  YEARS: document.querySelector('.count__years'),
   BUTTON: document.querySelector('.count__btn'),
   INPUT: document.querySelector('.count__input'),
-}
+};
 
 function getTimeRemaining(endtime) {
-  // if (isNaN(endtime)) {
-  //   return;
-  // }
   const remainingTime = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = millisecondsToSeconds(remainingTime);
+  const minutes = millisecondsToMinutes(remainingTime);
   const hours = millisecondsToHours(remainingTime);
   const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-  const years = isLeapYear(getYear(parseISO(endtime))) ?
-    Math.floor(remainingTime / (1000 * 60 * 60 * 24 * 366)) :
-    Math.floor(remainingTime / (1000 * 60 * 60 * 24 * 365));
   return {
     remainingTime,
+    seconds,
+    minutes,
     hours,
     days,
-    years
   };
 }
 
 function displayTimeleft() {
   const deadline = UI_ELEM.INPUT.value;
 
-  if (isPast(parseISO(deadline))) {
+  if (!deadline || isPast(parseISO(deadline))) {
     return;
   }
   const remainsDate = getTimeRemaining(deadline);
-  console.log(remainsDate);
 
+  UI_ELEM.SECONDS.innerText = remainsDate.seconds;
+  UI_ELEM.MINUTES.innerText = remainsDate.minutes;
   UI_ELEM.HOURS.innerText = remainsDate.hours;
   UI_ELEM.DAYS.innerText = remainsDate.days;
-  UI_ELEM.YEARS.innerText = remainsDate.years;
 }
 
 function setCountDown() {
   clearInterval(countDown);
 
-  displayTimeleft()
+  displayTimeleft();
   const deadlines = UI_ELEM.INPUT.value;
   const remainsDates = getTimeRemaining(deadlines);
   countDown = setInterval(() => {
@@ -56,8 +55,8 @@ function setCountDown() {
       clearInterval(countDown);
       return;
     }
-    displayTimeleft()
-  }, 3600000);
+    displayTimeleft();
+  }, 1000);
 }
 
 UI_ELEM.BUTTON.addEventListener('click', setCountDown);
